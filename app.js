90bd947bce6d2e8319a4695f30dbf427e4758a8b
Sagem4n <example@gmail.com>
2024-08-05 18:27:29 +0500

    practice

--- app.js
+++ app.js
@@ -1,29 +1,70 @@
-"use strict";
+// const ism = prompt("Ismingizni kiriting: ");
+// const yosh = +prompt("Yoshingizni kiriting");
+
+// let tugilganYil = 2022 - yosh;
+// let oy = yosh * 12;
+// let hafta = ((yosh * 365) / 7).toFixed(0);
+// let kun = yosh * 365;
+// let soat = kun * 24;
+// let daqiqa = soat * 60;
+// let soniya = daqiqa* 60;
+
+// const result = `Qadirli ${ism}.Siz ${tugilganYil} yilda tug'ilgansiz va siz ${oy} oy, ${hafta} hafta, ${kun} kun, ${soat} soat, ${daqiqa} daqiqa, ${soniya} soniya yashagansiz.`
+
+// alert(result)
+
+// const ism = prompt("Ism kiriting: ")
+// const ismlar = [`Yusuf`, `Sardor`, `Ali`]
+
+// if(ismlar.includes(ism)){
+//     alert(`ha ${ism} bor`)
+// } else {
+//     alert(`Afsus ${ism} yoq`)
+// }
+
+//FOR
+// const ismlar = [`Yusuf`, `Sardor`, `Ali`]
+
+// for (let i = 0; i < ismlar.length; i++)  {
+//     let result = ismlar[i] + "bek"
+//     console.log(result)
+// }
+
+//WHILE
+// const ismlar = [`Yusuf`, `Sardor`, `Ali`]
+// while (i < ismlar.length) {
+//     let result = ismlar[i] + "bek"
+//     console.log(result)
+//     i++
+// }
 
 
-//function desperation
-const sumOfAandB = calc(10,5);
-console.log(sumOfAandB);
+// const viloyatNomi = prompt("qaysi viloyat kerak ?")
 
-function cals(a,b) {
-    return a  + b;
-}
+// switch(viloyatNomi) {
+//     case "fargona":
+//         alert(" fargona viloyatida 2 mln aholi bor")
+//         break
+//     case "Andijon":
+//         alert(" andijon viloyatida 4 mln aholi bor")
+//         break
+//     case "navoi":
+//         alert(" navoi viloyatida 40 mln aholi bor")
+//         break
+//     case "namangan":
+//         alert(" fargona viloyatida 20 mln aholi bor")
+//         break
+//     default:
+//         alert(`Hozirda bazada ${viloyatNomi} yoq`)
+// }
 
 
-//function experation
-const logger = function () {
-    console.log("Hellow world");
-};
-logger();
+// const belgilar = prompt("Nimadirlar yozing...")
 
+// let meyor = 20;
 
-
-// arrow function
-const calc = (a, b) => a + b;
-
-console.log(calc(10, 5));
-
-const calc = (a) => {
-    console.log("!");
-    return a;
-};
+// if(belgilar.length > meyor) {
+//     alert(`siz ${meyor} ta belgi kiritishingiz mumkin,lekin hozirda siz ${belgilar.length} ta belgi kirtdiz.bu meyordan ${belgilar.length - meyor} ta ko'p`,)
+// } else{
+//     alert(`siz ${meyor} ta belgi kiritishingiz mumkin,va hozirda siz ${belgilar.length} ta belgi kirtdiz.yana  ${meyor - belgilar.length} ta kiritasiz`,)
+// }
